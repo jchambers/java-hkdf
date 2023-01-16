f00bbe53839523c4dd20f7fd29321e45102eb9f9
Jon Chambers <jon.chambers@gmail.com>
2023-01-15 22:31:05 -0500

    Consolidate extract/expand steps into a single "derive key" method

--- src/main/java/com/eatthepath/hkdf/HKDF.java
+++ src/main/java/com/eatthepath/hkdf/HKDF.java
@@ -38,66 +38,70 @@ public class HKDF {
         }
     }
 
-    public byte[] deriveKey(final byte[] salt, final byte[] inputKeyMaterial, final int outputKeyLength, final byte[] info) {
-        try {
-            final Mac hmac = Mac.getInstance(algorithm);
-            return expand(hmac, extract(hmac, salt, inputKeyMaterial), outputKeyLength, info);
-        } catch (final NoSuchAlgorithmException e) {
-            throw new AssertionError("Previously-legal algorithms must remain legal");
-        }
-    }
-
     public String getAlgorithm() {
         return algorithm;
     }
 
-    byte[] extract(final Mac hmac, final byte[] salt, final byte[] inputKeyMaterial) {
-        try {
-            hmac.init(salt != null && salt.length != 0 ? new SecretKeySpec(salt, algorithm) : defaultSalt);
-            return hmac.doFinal(inputKeyMaterial);
-        } catch (final InvalidKeyException e) {
-            // Technically, this can never happen because HmacSHA256 allows keys of any length even if very long keys
-            // are "discouraged" and very short keys are "a terrible, terrible idea"
-            throw new IllegalArgumentException(e);
+    public byte[] deriveKey(final byte[] salt, final byte[] inputKeyMaterial, final int outputKeyLength, final byte[] info) {
+        if (outputKeyLength < 1) {
+            throw new IllegalArgumentException("Output key length must be positive");
         }
-    }
 
-    byte[] expand(final Mac hmac, final byte[] pseudoRandomKey, final int outputKeyLength, final byte[] info) {
         if (outputKeyLength > 255 * macLength) {
-            throw new IllegalArgumentException("TODO");
-        }
-
-        final int rounds = (outputKeyLength + macLength - 1) / macLength;
-        final byte[] outputKey = new byte[rounds * macLength];
-
-        try {
-            hmac.init(new SecretKeySpec(pseudoRandomKey, algorithm));
-        } catch (final InvalidKeyException e) {
-            // This should never happen; this method should only be called with keys derived from the extract() method
-            throw new AssertionError("Extracted keys must be valid");
+            throw new IllegalArgumentException("Output key length with " + algorithm + " must be no more than " + (255 * macLength) + " bytes");
         }
 
         try {
-            hmac.update(info);
-            hmac.update((byte) 1);
-            hmac.doFinal(outputKey, 0);
+            final Mac hmac = Mac.getInstance(algorithm);
 
-            for (byte round = 1; round < rounds; round++) {
-                hmac.update(outputKey, (round - 1) * macLength, macLength);
-                hmac.update(info);
-                hmac.update((byte) (round + 1));
-                hmac.doFinal(outputKey, round * macLength);
+            // "Extract" a pseudo-random key
+            final Key pseudoRandomKey;
+
+            try {
+                hmac.init(salt != null && salt.length != 0 ? new SecretKeySpec(salt, algorithm) : defaultSalt);
+                pseudoRandomKey = new SecretKeySpec(hmac.doFinal(inputKeyMaterial), algorithm);
+            } catch (final InvalidKeyException e) {
+                // Technically, this can never happen because HmacSHA256 allows keys of any non-zero length even if very
+                // long keys are "discouraged" and very short keys are "a terrible, terrible idea"
+                throw new IllegalArgumentException(e);
             }
 
-            final byte[] truncatedKey = new byte[outputKeyLength];
-            System.arraycopy(outputKey, 0, truncatedKey, 0, outputKeyLength);
+            // "Expand" the pseudo-random key into output key material of the desired length
+            final int rounds = (outputKeyLength + macLength - 1) / macLength;
+            final byte[] outputKey = new byte[rounds * macLength];
 
-            return truncatedKey;
-        } catch (final ShortBufferException e) {
-            // This should never happen; we control the buffer and can be confident we've allocated enough space
-            throw new AssertionError(e);
-        } finally {
-            Arrays.fill(outputKey, (byte) 0);
+            try {
+                hmac.init(pseudoRandomKey);
+            } catch (final InvalidKeyException e) {
+                // This should never happen; we just derived the key and know it's valid
+                throw new AssertionError("Extracted keys must be valid");
+            }
+
+            try {
+                hmac.update(info);
+                hmac.update((byte) 1);
+                hmac.doFinal(outputKey, 0);
+
+                for (byte round = 1; round < rounds; round++) {
+                    hmac.update(outputKey, (round - 1) * macLength, macLength);
+                    hmac.update(info);
+                    hmac.update((byte) (round + 1));
+                    hmac.doFinal(outputKey, round * macLength);
+                }
+
+                final byte[] truncatedKey = new byte[outputKeyLength];
+                System.arraycopy(outputKey, 0, truncatedKey, 0, outputKeyLength);
+
+                return truncatedKey;
+            } catch (final ShortBufferException e) {
+                // This should never happen; we control the buffer and can be confident we've allocated enough space
+                throw new AssertionError(e);
+            } finally {
+                Arrays.fill(outputKey, (byte) 0);
+            }
+        } catch (final NoSuchAlgorithmException e) {
+            // We instantiated the Mac in the constructor; if it was legal then, it must be legal now
+            throw new AssertionError("Previously-legal algorithms must remain legal");
         }
     }
 }
