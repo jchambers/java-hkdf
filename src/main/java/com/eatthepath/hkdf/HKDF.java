4af61a71ee138dba53b30180de9bd9dacb3d7f0a
Jon Chambers <jon.chambers@gmail.com>
2023-02-18 11:34:55 -0500

    Once again offer "extract pseudo-random key" as an (optional!) stand-alone operation

--- src/main/java/com/eatthepath/hkdf/HKDF.java
+++ src/main/java/com/eatthepath/hkdf/HKDF.java
@@ -47,6 +47,38 @@ public class HKDF {
                             final byte[] info,
                             final int outputKeyLength) {
 
+        final Mac hmac;
+
+        try {
+            hmac = Mac.getInstance(algorithm);
+        } catch (final NoSuchAlgorithmException e) {
+            // We instantiated the Mac in the constructor; if it was legal then, it must be legal now
+            throw new AssertionError("Previously-legal algorithms must remain legal");
+        }
+
+        return deriveKey(extractPseudoRandomKey(inputKeyMaterial, salt, hmac), info, outputKeyLength, hmac);
+    }
+
+    public byte[] deriveKey(final byte[] pseudoRandomKey,
+                            final byte[] info,
+                            final int outputKeyLength) {
+
+        try {
+            return deriveKey(new SecretKeySpec(pseudoRandomKey, algorithm),
+                    info,
+                    outputKeyLength,
+                    Mac.getInstance(algorithm));
+        } catch (final NoSuchAlgorithmException e) {
+            // We instantiated the Mac in the constructor; if it was legal then, it must be legal now
+            throw new AssertionError("Previously-legal algorithms must remain legal");
+        }
+    }
+
+    private byte[] deriveKey(final Key pseudoRandomKey,
+                             final byte[] info,
+                             final int outputKeyLength,
+                             final Mac hmac) {
+
         if (outputKeyLength < 1) {
             throw new IllegalArgumentException("Output key length must be positive");
         }
@@ -56,57 +88,61 @@ public class HKDF {
                     "Output key length with " + algorithm + " must be no more than " + (255 * macLength) + " bytes");
         }
 
+        final int rounds = (outputKeyLength + macLength - 1) / macLength;
+        final byte[] outputKey = new byte[rounds * macLength];
+
         try {
-            final Mac hmac = Mac.getInstance(algorithm);
+            hmac.init(pseudoRandomKey);
+        } catch (final InvalidKeyException e) {
+            // This should never happen; we just derived the key and know it's valid
+            throw new AssertionError("Extracted keys must be valid");
+        }
 
-            // "Extract" a pseudo-random key
-            final Key pseudoRandomKey;
+        try {
+            hmac.update(info);
+            hmac.update((byte) 1);
+            hmac.doFinal(outputKey, 0);
 
-            try {
-                hmac.init(salt != null && salt.length != 0 ? new SecretKeySpec(salt, algorithm) : defaultSalt);
-                pseudoRandomKey = new SecretKeySpec(hmac.doFinal(inputKeyMaterial), algorithm);
-            } catch (final InvalidKeyException e) {
-                // Technically, this can never happen because HmacSHA256 allows keys of any non-zero length even if very
-                // long keys are "discouraged" and very short keys are "a terrible, terrible idea"
-                throw new IllegalArgumentException(e);
-            }
-
-            // "Expand" the pseudo-random key into output key material of the desired length
-            final int rounds = (outputKeyLength + macLength - 1) / macLength;
-            final byte[] outputKey = new byte[rounds * macLength];
-
-            try {
-                hmac.init(pseudoRandomKey);
-            } catch (final InvalidKeyException e) {
-                // This should never happen; we just derived the key and know it's valid
-                throw new AssertionError("Extracted keys must be valid");
-            }
-
-            try {
+            for (int round = 1; round < rounds; round++) {
+                hmac.update(outputKey, (round - 1) * macLength, macLength);
                 hmac.update(info);
-                hmac.update((byte) 1);
-                hmac.doFinal(outputKey, 0);
-
-                for (int round = 1; round < rounds; round++) {
-                    hmac.update(outputKey, (round - 1) * macLength, macLength);
-                    hmac.update(info);
-                    hmac.update((byte) (round + 1));
-                    hmac.doFinal(outputKey, round * macLength);
-                }
-
-                final byte[] truncatedKey = new byte[outputKeyLength];
-                System.arraycopy(outputKey, 0, truncatedKey, 0, outputKeyLength);
-
-                return truncatedKey;
-            } catch (final ShortBufferException e) {
-                // This should never happen; we control the buffer and can be confident we've allocated enough space
-                throw new AssertionError(e);
-            } finally {
-                Arrays.fill(outputKey, (byte) 0);
+                hmac.update((byte) (round + 1));
+                hmac.doFinal(outputKey, round * macLength);
             }
+
+            final byte[] truncatedKey = new byte[outputKeyLength];
+            System.arraycopy(outputKey, 0, truncatedKey, 0, outputKeyLength);
+
+            return truncatedKey;
+        } catch (final ShortBufferException e) {
+            // This should never happen; we control the buffer and can be confident we've allocated enough space
+            throw new AssertionError(e);
+        } finally {
+            Arrays.fill(outputKey, (byte) 0);
+        }
+    }
+
+    public byte[] extractPseudoRandomKey(final byte[] inputKeyMaterial,
+                                         final byte[] salt) {
+
+        try {
+            return extractPseudoRandomKey(inputKeyMaterial, salt, Mac.getInstance(algorithm)).getEncoded();
         } catch (final NoSuchAlgorithmException e) {
             // We instantiated the Mac in the constructor; if it was legal then, it must be legal now
             throw new AssertionError("Previously-legal algorithms must remain legal");
+        }
+    }
+
+    private Key extractPseudoRandomKey(final byte[] inputKeyMaterial,
+                                       final byte[] salt,
+                                       final Mac hmac) {
+
+        try {
+            hmac.init(salt != null && salt.length != 0 ? new SecretKeySpec(salt, algorithm) : defaultSalt);
+            return new SecretKeySpec(hmac.doFinal(inputKeyMaterial), algorithm);
+        } catch (final InvalidKeyException e) {
+            // Practically, this should never happen for any hashing algorithm (barring zero-length input key material)
+            throw new IllegalArgumentException(e);
         }
     }
 }
--- src/test/java/com/eatthepath/hkdf/HKDFTest.java
+++ src/test/java/com/eatthepath/hkdf/HKDFTest.java
@@ -26,6 +26,70 @@ class HKDFTest {
 
     @ParameterizedTest
     @MethodSource
+    void extractPseudoRandomKey(final String algorithm,
+                                final byte[] inputKeyMaterial,
+                                final byte[] salt,
+                                final byte[] expectedKey) throws NoSuchAlgorithmException {
+
+        assertArrayEquals(expectedKey, new HKDF(algorithm).extractPseudoRandomKey(inputKeyMaterial, salt));
+    }
+
+    private static Stream<Arguments> extractPseudoRandomKey() {
+        // Test vectors from https://www.rfc-editor.org/rfc/rfc5869.txt, Appendix A
+        return Stream.of(
+                Arguments.of(
+                        "HmacSHA256",
+                        parseHex("0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b"),
+                        parseHex("000102030405060708090a0b0c"),
+                        parseHex("077709362c2e32df0ddc3f0dc47bba6390b6c73bb50f9c3122ec844ad7c2b3e5")
+                ),
+
+                Arguments.of(
+                        "HmacSHA256",
+                        parseHex("000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f202122232425262728292a2b2c2d2e2f303132333435363738393a3b3c3d3e3f404142434445464748494a4b4c4d4e4f"),
+                        parseHex("606162636465666768696a6b6c6d6e6f707172737475767778797a7b7c7d7e7f808182838485868788898a8b8c8d8e8f909192939495969798999a9b9c9d9e9fa0a1a2a3a4a5a6a7a8a9aaabacadaeaf"),
+                        parseHex("06a6b88c5853361a06104c9ceb35b45cef760014904671014a193f40c15fc244")
+                ),
+
+                Arguments.of(
+                        "HmacSHA256",
+                        parseHex("0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b"),
+                        new byte[0],
+                        parseHex("19ef24a32c717b167f33a91d6f648bdf96596776afdb6377ac434c1c293ccb04")
+                ),
+
+                Arguments.of(
+                        "HmacSHA1",
+                        parseHex("0b0b0b0b0b0b0b0b0b0b0b"),
+                        parseHex("000102030405060708090a0b0c"),
+                        parseHex("9b6c18c432a7bf8f0e71c8eb88f4b30baa2ba243")
+                ),
+
+                Arguments.of(
+                        "HmacSHA1",
+                        parseHex("000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f202122232425262728292a2b2c2d2e2f303132333435363738393a3b3c3d3e3f404142434445464748494a4b4c4d4e4f"),
+                        parseHex("606162636465666768696a6b6c6d6e6f707172737475767778797a7b7c7d7e7f808182838485868788898a8b8c8d8e8f909192939495969798999a9b9c9d9e9fa0a1a2a3a4a5a6a7a8a9aaabacadaeaf"),
+                        parseHex("8adae09a2a307059478d309b26c4115a224cfaf6")
+                ),
+
+                Arguments.of(
+                        "HmacSHA1",
+                        parseHex("0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b"),
+                        new byte[0],
+                        parseHex("da8c8a73c7fa77288ec6f5e7c297786aa0d32d01")
+                ),
+
+                Arguments.of(
+                        "HmacSHA1",
+                        parseHex("0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c"),
+                        null,
+                        parseHex("2adccada18779e7c2077ad2eb19d3f3e731385dd")
+                )
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource
     void deriveKey(final String algorithm,
                    final byte[] inputKeyMaterial,
                    final byte[] salt,
